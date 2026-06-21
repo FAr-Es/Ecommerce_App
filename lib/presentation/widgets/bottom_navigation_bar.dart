08fbc4eb85104fc62644ab9db4f0ad68ffc0a84e
FAr-Es <farestarabek@outlook.com>
2026-06-21 23:09:25 +0300

    update bottom navigation bar icons to reflect selected state with color changes

--- lib/presentation/widgets/bottom_navigation_bar.dart
+++ lib/presentation/widgets/bottom_navigation_bar.dart
@@ -17,23 +17,23 @@ class CustomBottomNavigationBar extends StatelessWidget {
         unselectedItemColor: AppColors.secondaryColor,
         items: [
           BottomNavigationBarItem(
-            icon: Image.asset("assets/icons/home.png"),
+            icon: Image.asset("assets/icons/home.png", color: currentIndex == 0 ? AppColors.primaryColor : AppColors.secondaryColor),
             label: "Home",
           ),
           BottomNavigationBarItem(
-            icon: Image.asset("assets/icons/search.png"),
+            icon: Image.asset("assets/icons/search.png", color: currentIndex == 1 ? AppColors.primaryColor : AppColors.secondaryColor),
             label: "Search",
           ),
           BottomNavigationBarItem(
-            icon: Image.asset("assets/icons/saved.png"),
+            icon: Image.asset("assets/icons/saved.png", color: currentIndex == 2 ? AppColors.primaryColor : AppColors.secondaryColor),
             label: "Saved",
           ),
           BottomNavigationBarItem(
-            icon: Image.asset("assets/icons/cart.png"),
+            icon: Image.asset("assets/icons/cart.png", color: currentIndex == 3 ? AppColors.primaryColor : AppColors.secondaryColor),
             label: "Cart",
           ),
           BottomNavigationBarItem(
-            icon: Image.asset("assets/icons/account.png"),
+            icon: Image.asset("assets/icons/account.png", color: currentIndex == 4 ? AppColors.primaryColor : AppColors.secondaryColor),
             label: "Account",
           ),
         ],
